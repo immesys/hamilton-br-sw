15a09d7bbeb9e8932b265475291c920f36426996
Michael Andersen <michael@steelcode.com>
2017-04-27 15:24:00 -0700

    Tweaks

--- brgw/datalogger.go
+++ brgw/datalogger.go
@@ -1,25 +1,26 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/binary"
 	"fmt"
-	"io/ioutil"
-	"path"
+	"os"
 	"sync"
 	"syscall"
 	"time"
 
-  "github.com/immesys/hamilton-br-sw/brgw/pb"
+	"github.com/gogo/protobuf/proto"
+	"github.com/immesys/hamilton-br-sw/brgw/pb"
 )
 
 type dataLoggerCache struct {
 	lastFlush time.Time
-	messages  []*entry
+	messages  []LogEntry
 	sync.Mutex
 }
 
 type entry struct {
-	time    time.Time
-	payload []byte
+	payload LogEntry
 }
 
 var DLC *dataLoggerCache
@@ -32,8 +33,8 @@ func logFrameToDatalogger(f *egressframe) {
 
 }
 
-func getDir(t time.Time) string {
-	return t.Format("2006/01_Jan/02/15")
+func getFilename(t time.Time) string {
+	return t.Format("2006/01_Jan/02_Mon/15")
 }
 func flushDLCLoop() {
 	for {
@@ -57,11 +58,11 @@ func flushDLC() {
 	DLC.lastFlush = time.Now()
 	DLC.Unlock()
 
-	//Establish the marker file is there
-	dat, _ := ioutil.ReadFile(path.Join(datalogdir, "enable"))
-	if string(dat) != "enable_datalogger" {
-		fmt.Printf("Skipping DLC flush, not enabled")
-	}
+	// //Establish the marker file is there
+	// dat, _ := ioutil.ReadFile(path.Join(datalogdir, "enable"))
+	// if string(dat) != "enable_datalogger" {
+	// 	fmt.Printf("Skipping DLC flush, not enabled")
+	// }
 	//Establish there is space
 	var stat syscall.Statfs_t
 	err := syscall.Statfs(datalogdir, &stat)
@@ -71,11 +72,27 @@ func flushDLC() {
 	}
 
 	if stat.Bavail*stat.Bsize < MinAvailSpace {
-		fmt.Printf("Skipping DLC flush, no space")
+		fmt.Printf("Skipping DLC flush, no space\n")
 		return
 	}
 
 	ls := pb.LogSet{}
-  ls.
+	//populate
+	data, err := proto.Marshal(ls)
+	if err != nil {
+		fmt.Printf("marshaling error: \n", err)
+		return
+	}
+	sum := md5.Sum(data)
+	sz := len(data)
 
+	fname := getFilename(ls.Time())
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	hdr := make([]byte, 8+4+16)
+	copy(hdr[0:8], TOKEN)
+	binary.LittleEndian.PutUint32(hdr[8:12], sz)
+	copy(hdr[12:], sum)
+	f.Write(hdr)
+	f.Write(data)
+	f.Close()
 }
